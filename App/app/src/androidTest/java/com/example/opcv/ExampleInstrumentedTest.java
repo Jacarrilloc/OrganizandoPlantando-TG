8a3017e823f92260182ea3fa391956f80a59892a
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-01-30 13:55:39 -0500

    Creacion de Actividad Login
    Se realiza crea la actividad de Login para la aplicacion pero sin el diseño ni la implementacion propuesta propuesto

--- App/app/src/androidTest/java/com/example/opcv/ExampleInstrumentedTest.java
+++ App/app/src/androidTest/java/com/example/opcv/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.tg.opcv;
+package com.example.opcv;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.tg.opcv", appContext.getPackageName());
+        assertEquals("com.example.opcv", appContext.getPackageName());
     }
 }
--- App/app/src/main/java/com/example/opcv/MainActivity.java
+++ App/app/src/main/java/com/example/opcv/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tg.opcv;
+package com.example.opcv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
