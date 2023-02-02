d8653265a8a490547cd68fb50fa83cf91698d37b
heyli <heylingburgos@gmail.com>
2023-02-02 15:38:31 -0500

    Add new activities

---
+++ App/app/src/main/java/com/example/opcv/GenerateReportsActivity.java
@@ -0,0 +1,14 @@
+package com.example.opcv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateReportsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_reports);
+    }
+}
