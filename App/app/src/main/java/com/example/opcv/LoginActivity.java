ee46172d73315899f6d65efdaff6086260e82e45
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-02-01 23:51:33 -0500

    Modificacion a Actividad de Login
    Se realizan cambios esteticos

--- App/app/src/main/java/com/example/opcv/LoginActivity.java
+++ App/app/src/main/java/com/example/opcv/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.opcv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button registerButtom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerButtom = findViewById(R.id.registerButtom);
+
+        registerButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent registerActivityLaunch = new Intent(this,);
+            }
+        });
     }
 }
