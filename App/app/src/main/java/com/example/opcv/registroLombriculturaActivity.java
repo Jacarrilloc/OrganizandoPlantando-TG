2615762684d3e32a6fc6f84a7d606f108385095f
DanielCamiloFierroGarcia <dacafiga1@hotmail.com>
2023-02-01 19:46:29 -0500

    Crear actividad registro lombricultura

---
+++ App/app/src/main/java/com/example/opcv/registroLombriculturaActivity.java
@@ -0,0 +1,14 @@
+package com.example.opcv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registroLombriculturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_lombricultura);
+    }
+}
