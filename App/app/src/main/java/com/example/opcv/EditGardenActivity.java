e26274cbe772d985ca07eff042fe5752867deb64
heyli <heylingburgos@gmail.com>
2023-02-02 15:22:46 -0500

    Add new activities

--- App/app/src/main/java/com/example/opcv/EditGardenActivity.java
+++ App/app/src/main/java/com/example/opcv/EditGardenActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GardenEditActivity extends AppCompatActivity {
+public class EditGardenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_garden_edit);
+        setContentView(R.layout.activity_edit_garden);
     }
 }
