44ee3914c510cd842695483840a2a01f0e301d19
heyli <heylingburgos@gmail.com>
2023-02-02 15:17:19 -0500

    Add new activities

---
+++ App/app/src/main/java/com/example/opcv/guestLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.opcv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class guestLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_login);
+    }
+}
