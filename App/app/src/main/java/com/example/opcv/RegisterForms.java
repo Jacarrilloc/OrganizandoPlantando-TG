a955814abecfdea0dd9de5a1796b38277fb2a067
heyli <heylingburgos@gmail.com>
2023-02-03 12:38:48 -0500

    Add new activities

---
+++ App/app/src/main/java/com/example/opcv/RegisterForms.java
@@ -0,0 +1,14 @@
+package com.example.opcv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterForms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_forms);
+    }
+}
