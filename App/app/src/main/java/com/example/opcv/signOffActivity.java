a0ae78768d607f1839617f0d6ff961cd2ec515ce
DanielCamiloFierroGarcia <dacafiga1@hotmail.com>
2023-02-15 22:42:45 -0500

    Cerrar sesion y borrar cuenta listo

--- App/app/src/main/java/com/example/opcv/signOffActivity.java
+++ App/app/src/main/java/com/example/opcv/signOffActivity.java
@@ -2,13 +2,43 @@ package com.example.opcv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class signOffActivity extends AppCompatActivity {
+    private Button returnScreen, signOff;
+
+    private FirebaseAuth autentication;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_off);
+
+        autentication = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+
+        returnScreen = (Button) findViewById(R.id.returnButton2);
+        returnScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(signOffActivity.this, EditUserActivity.class));
+            }
+        });
+
+        signOff = (Button) findViewById(R.id.closeButton);
+        signOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                autentication.signOut();
+                startActivity(new Intent(signOffActivity.this, NewToAppActivity.class));
+            }
+        });
     }
 }
