9681ac8e66757d996fed15bfb8eefe17024ac14f
heyli <heylingburgos@gmail.com>
2023-03-07 17:24:13 -0500

    Creación formularios 1 al 4

---
+++ App/app/src/main/java/com/example/opcv/formsScreen/Form_IMP.java
@@ -0,0 +1,17 @@
+package com.example.opcv.formsScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.opcv.R;
+
+//Formulario Inventario de Materia Prima
+public class Form_IMP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_imp);
+    }
+}
