4235353ef35995047ab77b468e1685c0d9203939
heyli <heylingburgos@gmail.com>
2023-03-10 16:00:40 -0500

    Adición funcionalidad de mostrar huertas disponibles

--- App/app/src/main/java/com/example/opcv/HomeActivity.java
+++ App/app/src/main/java/com/example/opcv/HomeActivity.java
@@ -102,6 +102,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
