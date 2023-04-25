669a022d50c4ac821d99e56201eced0c7e6a1fab
Goldendragonfist <dacafiga1@hotmail.com>
2023-04-25 14:45:52 -0500

    Correccion notificaciones

--- App/app/src/main/java/com/example/opcv/notifications/Notifications.java
+++ App/app/src/main/java/com/example/opcv/notifications/Notifications.java
@@ -2,23 +2,87 @@ package com.example.opcv.notifications;
 
 import static androidx.core.content.ContextCompat.getSystemService;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.opcv.R;
 
+import java.security.Permissions;
+
 public class Notifications {
-    public void notification(String title, String text, Context context){
+    public void notification(String title, String text, Context context) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Ceres Channel";
+            String description = "Ceres app notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("channel_id", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+            // Crear la notificación
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "channel_id")
+                    .setSmallIcon(R.drawable.im_logo_ceres_green)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+            // Mostrar la notificación
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+       
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                //ActivityCompat.requestPermissions(context, Permissions.);
+                return;
+            }
+            int notificationId = 1;
+            notificationManagerCompat.notify(notificationId, builder.build());
+        }
+        else {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        /*Intent intent1 = new Intent(context, intent.getClass());
+        intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);*/
+
+
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
+                    .setSmallIcon(R.drawable.im_logo_ceres_green)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                    //.setContentIntent(pendingIntent)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setAutoCancel(true);
+            Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            builder.setSound(soundUri);
+            int notificationId = 1;
+            notificationManager.notify(notificationId, builder.build());
+        }
+
+
+    }
+    public void notification1(String title, String text, Context context){
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         /*Intent intent1 = new Intent(context, intent.getClass());
@@ -26,7 +90,8 @@ public class Notifications {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);*/
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "dafault")
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
                 .setSmallIcon(R.drawable.im_logo_ceres_green)
                 .setContentTitle(title)
                 .setContentText(text)
@@ -34,8 +99,8 @@ public class Notifications {
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 //.setContentIntent(pendingIntent)
                 .setAutoCancel(true);
-       Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-       builder.setSound(soundUri);
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(soundUri);
         int notificationId = 1;
         notificationManager.notify(notificationId, builder.build());
     }
