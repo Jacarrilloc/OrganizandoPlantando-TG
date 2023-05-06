1e76ada307a874694f2493c15c615702cf2cc3d7
heyli <heylingburgos@gmail.com>
2023-05-06 14:26:04 -0500

    Corrections in create garden and other layouts

---
+++ App/app/src/main/java/com/example/opcv/view/ludification/ShowInfoLvl2Activity.java
@@ -0,0 +1,16 @@
+package com.example.opcv.view.ludification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.opcv.R;
+
+public class ShowInfoLvl2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_info_lvl2);
+    }
+}
